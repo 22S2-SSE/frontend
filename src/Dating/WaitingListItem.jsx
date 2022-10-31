db2eb40ee2889ba04a4e751d10b1345ebdae1948
Ivens_Zhang <ivensz98@gmail.com>
2022-11-01 00:33:41 +1030

    add hover style on waiting list item

--- src/Dating/WaitingListItem.jsx
+++ src/Dating/WaitingListItem.jsx
@@ -1,6 +1,6 @@
 const WaitingListItem = ({name, message, image}) => {
   return (
-    <div className='flex mb-4 h-20 space-x-4 w-full'>
+    <div className='flex mb-4 px-2 py-1 rounded-md h-20 space-x-4 w-full hover:bg-slate-100'>
       <img src={image} alt="" className=' rounded-full' />
       <div className='flex flex-col justify-evenly'>
         <div className=' text-xl font-semibold text-left'>
