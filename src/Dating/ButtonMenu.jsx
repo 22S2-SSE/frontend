0c2a31d0e7bf7d8eec6728d84e0bffe8486220ae
Ivens_Zhang <ivensz98@gmail.com>
2022-11-06 21:53:43 +1030

    Add button click event on dating page

--- src/Dating/ButtonMenu.jsx
+++ src/Dating/ButtonMenu.jsx
@@ -5,11 +5,11 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import CloseIcon from '@mui/icons-material/Close';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ReplayIcon from '@mui/icons-material/Replay';
-const ButtonMenu = () => {
+const ButtonMenu = ({changeImage, changeImage2}) => {
   return (
     <div className=" flex justify-center space-x-5 relative -top-16">
       <div className=' bg-slate-50 w-14 h-14 rounded-full flex flex-col justify-center'>
-        <IconButton aria-label="delete">
+        <IconButton aria-label="delete" onClick={changeImage}>
           <CloseIcon fontSize="large"/>
         </IconButton>
       </div>
@@ -19,7 +19,7 @@ const ButtonMenu = () => {
       </IconButton>
       </div>
       <div className=' bg-slate-50 w-14 h-14 rounded-full flex flex-col justify-center'>
-      <IconButton color="primary" aria-label="add to shopping cart" >
+      <IconButton color="primary" aria-label="add to shopping cart" onClick={changeImage2} >
         <FavoriteIcon color="error" fontSize="large"/>
       </IconButton>
       </div>
--- src/Dating/index.jsx
+++ src/Dating/index.jsx
@@ -1,47 +1,102 @@
-import WaitingListItem from "./WaitingListItem"
-import ButtonMenu from './ButtonMenu'
-import a from '../assets/dating/girls/a.jpg'
-import b from '../assets/dating/girls/b.jpg'
-import d from '../assets/dating/girls/d.jpg'
-import c from '../assets/dating/girls/c.jpg'
-import e from '../assets/dating/girls/e.jpg'
+import WaitingListItem from "./WaitingListItem";
+import ButtonMenu from "./ButtonMenu";
+import a from "../assets/dating/girls/a.jpg";
+import b from "../assets/dating/girls/b.jpg";
+import d from "../assets/dating/girls/d.jpg";
+import c from "../assets/dating/girls/c.jpg";
+import e from "../assets/dating/girls/e.jpg";
+
+import profiles from "../Profile/profile.json";
+import profiles2 from "../Profile/profile2.json";
+import profiles3 from "../Profile/profile3.json";
+
+import { useState } from "react";
 
 const Dating = () => {
+  const [profileList, setProfileList] = useState(profiles)
+
+  const renderProfileItems = (profiles) => {
+    let content = [];
+    for (const profile in profiles) {
+      content.push(
+        <div
+      className="flex justify-around items-center font-semibold rounded-lg mt-2 h-10 hover:bg-gray-200 bg-gray-300">
+      <div className=" text-left w-36">{profile}</div>
+      <div className=" text-left w-36">{profiles[profile]}</div>
+    </div>
+      )
+    }
+    return content;
+  };
+
+  const [imageSrc, setImageSrc] = useState(a)
+
+  const changeImage = () => {
+    setImageSrc(b)
+    setProfileList(profiles2)
+  }
+  const changeImage2 = () => {
+    setImageSrc(c)
+    setProfileList(profiles3)
+  }
+
   return (
     <div className="">
       {/* Header */}
-      <div className="w-full h-20 bg-gradient-to-b from-blue-300 " style={{
-        minHeight: "25vh",
-      }}></div>
+      <div
+        className="w-full h-20 bg-gradient-to-b from-blue-300 "
+        style={{
+          minHeight: "25vh",
+        }}
+      ></div>
       {/* Content */}
       <div className="flex px-10">
         {/* Waiting List */}
         <div className=" basis-1/4">
-          <WaitingListItem image={a} name={"Anika"} message={"Oh i don't like fish"}/>
-          <WaitingListItem image={b} name={"Lili"} message={"I really miss you."}/>
-          <WaitingListItem image={d} name={"Shreya"} message={"Hello"}/>
-          <WaitingListItem image={c} name={"Lilly"} message={"If I were a stop light, I’d turn"}/>
-          <WaitingListItem image={e} name={"Monika"} message={"How about a movie and then…"}/>
+          <WaitingListItem
+            image={a}
+            name={"Anika"}
+            message={"Oh i don't like fish"}
+          />
+          <WaitingListItem
+            image={b}
+            name={"Lili"}
+            message={"I really miss you."}
+          />
+          <WaitingListItem
+            image={c}
+            name={"Lilly"}
+            message={"If I were a stop light, I’d turn"}
+          />
+          <WaitingListItem image={d} name={"Shreya"} message={"Hello"} />
+          <WaitingListItem
+            image={e}
+            name={"Monika"}
+            message={"How about a movie and then…"}
+          />
         </div>
 
         {/* Images */}
         <div className=" basis-1/2">
-          <img src={a} alt="" className=" mx-auto" style={{
-            width: '550px',
-            height: '550px'
-          }}/>
-          <ButtonMenu />
+          <img
+            src={imageSrc}
+            alt=""
+            className=" mx-auto"
+            style={{
+              width: "550px",
+              height: "550px",
+            }}
+          />
+          <ButtonMenu changeImage={changeImage} changeImage2={changeImage2}/>
         </div>
 
         {/* Detail Information */}
-        <div className=" basis-1/4 bg-slate-400">
-          3
+        <div className=" basis-1/4">
+          {renderProfileItems(profileList)}
         </div>
-
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dating
+export default Dating;
